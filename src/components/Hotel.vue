51c81d2362b5eac1d18c194109d3b71873278a6b
David Blandón <djesus1906@protonmail.com>
2022-10-20 20:10:27 +0200

    Finished demo

---
+++ src/components/Hotel.vue
@@ -0,0 +1,24 @@
+<script setup>
+defineProps({
+  title: {
+    required: true,
+    type: String,
+  },
+});
+</script>
+
+<template>
+  <p class="title">{{ title }}</p>
+</template>
+
+<style scoped>
+.title {
+  font-size: 20px;
+  line-height: 1.6;
+  border: 1px solid rgba(var(--accent), 25%);
+  background-color: white;
+  padding: 1rem;
+  border-radius: 0.4rem;
+  color: rgb(var(--accent));
+}
+</style>
---
+++ src/components/HotelList.vue
@@ -0,0 +1,24 @@
+<script setup>
+import Hotel from "./Hotel.vue";
+defineProps({
+  hotels: {
+    default: [],
+    type: Array,
+  },
+});
+</script>
+
+<template>
+  <ul class="hotel-list">
+    <Hotel v-for="h in hotels" :title="h.title.rendered"></Hotel>
+  </ul>
+</template>
+
+<style scoped>
+.hotel-list {
+  border: 1px solid rgb(var(--accent));
+  border-radius: 10px;
+  filter: drop-shadow(40px);
+  padding: 0 15px;
+}
+</style>
